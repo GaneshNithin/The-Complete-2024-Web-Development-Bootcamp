57c63a55ec94d4594bef8d739a46e100399451e1
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-06-26 10:05:08 +0530

    refactor: add section 16 DOM challenge.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- Section 16 - DOM/DOM Challenge Starting Files/index.js
+++ Section 16 - DOM/DOM Challenge Starting Files/index.js
@@ -1,2 +1,6 @@
 const list = document.querySelector("ul");
 list.lastElementChild.innerHTML = "I changed you!";
+
+const aTag = document.querySelector("li a");
+console.log(aTag);
+aTag.style.color = "red";
